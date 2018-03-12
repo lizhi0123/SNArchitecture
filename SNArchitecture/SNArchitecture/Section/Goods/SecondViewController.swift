d1c966b343d17c5063285174a88eecdaa2ab0f51
Sunny <zzl@zhaioto.com>
2018-03-12 10:59:47 +0800

    添加主要文件夹

---
+++ SNArchitecture/SNArchitecture/Section/Goods/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SNArchitecture
+//
+//  Created by Sunny on 12/3/18.
+//  Copyright © 2018年 Sunny. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SNArchitecture/SNArchitecture/Section/Home/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SNArchitecture
+//
+//  Created by Sunny on 12/3/18.
+//  Copyright © 2018年 Sunny. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
